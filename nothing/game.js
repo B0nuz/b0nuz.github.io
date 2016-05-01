2933b0b597ad24983177f5763b56dc150fcd9bba
Bonus <wrotynski23@gmail.com>
2016-05-01 21:40:12 +0200

    nothing v0.15

--- nothing/game.js
+++ nothing/game.js
@@ -1,7 +1,12 @@
 var score = 0;
+var bAmmount = 0;
+var apc = 1;
+
+
 function displayScore()
 {
 	document.getElementById('score').innerHTML = score +' agatek';
+	document.getElementById('apcDisplay').innerHTML = apc + ' apc';
 }
 
 var isAgatkaFlipped;
@@ -19,7 +24,38 @@ function agatkaClick()
 		agatka.style.transform = 'scale(-1, 1)';
 		isAgatkaFlipped = 1;
 	}
+	score++;
 
-	score += 1;
 	displayScore();
 }
+
+function buy1()
+{
+	var playground = document.getElementById('playground');
+	var price = 15 * (bAmmount * bAmmount * bAmmount + 1);
+	var futurePrice = 15 * ((bAmmount+1) * (bAmmount+1) * (bAmmount+1) + 1);
+	var bAmmountDisplay = document.getElementById('bAmmount');
+	var bPrice = document.getElementById('bPrice');
+	if( score >= price )
+	{
+		score -= price;
+		if(bAmmount==0)
+		{
+			playground.innerHTML += '<img src="img/1b.gif" alt="/b/">';
+		}
+		bAmmount++;
+		bAmmountDisplay.innerHTML = bAmmount;
+		bPrice.innerHTML = futurePrice + ' agatek';
+		displayScore();
+	}
+}
+
+
+
+
+
+window.setInterval(function(){
+  score += bAmmount;
+  displayScore();
+}, 5000);
+
